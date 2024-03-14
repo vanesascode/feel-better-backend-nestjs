a80382e1451d7dd6d0554aa4400d2e0b50b1ecd5
vanesa <vanesa.juarez.paris@gmail.com>
2024-03-14 11:23:56 +0100

    feat: :sparkles: add configuration of swagger, cors and pipes in main.ts file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('My API documentation')
+    .setDescription('My API description')
+    .setVersion('1.0')
+    .addTag('Auth')
+    .addTag('Items')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(3000);
 }
 bootstrap();
