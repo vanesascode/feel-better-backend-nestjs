f75aa49ae933edfb848f7bb22c80bd1819460028
vanesa <vanesa.juarez.paris@gmail.com>
2024-03-18 20:25:40 +0100

    feat: :sparkles: add AuthModule to the app module file

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { UserThoughtModule } from './userthoughts/userthought.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { UserThoughtModule } from './userthoughts/userthought.module';
       envFilePath: '.env',
     }),
     MongooseModule.forRoot(process.env.MONGODB_CONNECTION_STRING),
+    AuthModule,
     UserThoughtModule,
     UsersModule,
   ],
