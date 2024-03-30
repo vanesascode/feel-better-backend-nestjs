a09a0cef06e7f0ead0833872af80857fcc567cf4
vanesa <vanesa.juarez.paris@gmail.com>
2024-03-30 22:03:01 +0100

    feat: :sparkles: add encryption to the password when modified by the user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,6 +4,7 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User, UserDocument } from './schema/user.schema';
+import { hash } from 'bcrypt';
 
 @Injectable()
 export class UsersService {
@@ -27,7 +28,10 @@ export class UsersService {
   }
 
   async update(id: string, updateUserDto: UpdateUserDto) {
-    const user = await this.usersModel.findByIdAndUpdate(id, updateUserDto);
+    const { password } = updateUserDto;
+    const plainToHash = await hash(password, 10);
+    const updatedUserDto = { ...updateUserDto, password: plainToHash };
+    const user = await this.usersModel.findByIdAndUpdate(id, updatedUserDto);
     return user;
   }
 
