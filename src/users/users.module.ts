c778b92de322807e18d626d4c50a9de89d18556a
vanesa <vanesa.juarez.paris@gmail.com>
2024-04-19 15:59:49 +0200

    feat: :sparkles: add the UserThought schema to the UsersModule

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,10 +3,12 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './schema/user.schema';
+import { UserThought } from 'src/userthoughts/schema/userthought.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    MongooseModule.forFeature([{ name: UserThought.name, schema: UserSchema }]),
   ],
   controllers: [UsersController],
   providers: [UsersService],
